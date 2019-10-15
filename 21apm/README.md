150703577f19f8a884eaa9f5f7355315c5370001
Elliot Chen <elliot.chen@elliot.tw>
2019-10-15 15:29:40 +0800

    add apm sample

---
+++ 21apm/README.md
@@ -0,0 +1,12 @@
+# APM
+
+主要利用Prometheus + Grafana來展示基本的系統監控
+
+## build
+
+```
+mvn clean package dockerfile:build
+```
+
+
+
---
+++ 21apm/src/main/java/tw/elliot/apm/Application.java
@@ -0,0 +1,20 @@
+package tw.elliot.apm;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", "springapm");
+	}
+}
