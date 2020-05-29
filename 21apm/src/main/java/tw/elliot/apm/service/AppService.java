3b42412b3b3e86cc4ac0ca62a334777b9bae8c9d
Elliot Chen <elliot.chen@elliot.tw>
2020-05-29 17:17:24 +0800

    add build-info

---
+++ 21apm/src/main/java/tw/elliot/apm/service/AppService.java
@@ -0,0 +1,59 @@
+package tw.elliot.apm.service;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.FunctionCounter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author elliot
+ */
+@Service
+public class AppService implements InitializingBean {
+
+	@Autowired
+	private MeterRegistry meterRegistry;
+
+	private AtomicInteger onlineUser = new AtomicInteger(0);
+	//private AtomicInteger irsOrderCounter = new AtomicInteger(0);
+	//private AtomicInteger etsOrderCounter = new AtomicInteger(0);
+
+	private Gauge onlineUserGauge;
+
+	private Counter irsOrderCounter;
+	private Counter etsOrderCounter;
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		//(onlineUser)->{return onlineUser.intValue();}
+		onlineUserGauge = Gauge.builder("app.online.user", onlineUser, AtomicInteger::intValue).register(meterRegistry);
+
+		irsOrderCounter = Counter.builder("app.order")
+				.tag("service","irs").description("Order Count with Service[IRS]")
+				.register(meterRegistry);
+
+		etsOrderCounter = Counter.builder("app.order")
+				.tag("service","ets").description("Order Count with Service[ETS]")
+				.register(meterRegistry);
+	}
+
+	public void login() {
+		onlineUser.incrementAndGet();
+	}
+
+	public void logout() {
+		onlineUser.decrementAndGet();
+	}
+
+	public void irsOrder() {
+		irsOrderCounter.increment();
+	}
+
+	public void etsOrder() {
+		etsOrderCounter.increment();
+	}
+}
